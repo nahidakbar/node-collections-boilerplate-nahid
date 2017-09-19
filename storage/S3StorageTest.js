c69b17f3955b7c521a60fa6baca74e7c8238b6cb
GOD <nahid.akbar@data61.csiro.au>
2017-09-20 00:39:53 +1000

    save

--- storage/S3StorageTest.js
+++ storage/S3StorageTest.js
@@ -1,12 +1,57 @@
 "use strict";
 
-if (process.env.S3STORE_TEST_HOST && process.env.S3STORE_TEST_REGION)
-{
+var AWS = require('aws-sdk-mock');
+
+// set up a mock service
+const buckets = {};
+
+AWS.mock('S3', 'listObjectsV2', function (params, callback) {
+  console.log('LIST', params);
+  const bucket = buckets[params.Bucket] || {};
+  const Contents = JSON.parse(JSON.stringify(Object.values(bucket)));
+  Contents.forEach(item =>
+  {
+    item.LastModified = new Date(item.LastModified)
+  })
+  callback(null, {Contents});
+});
+
+AWS.mock('S3', 'getObject', function (params, callback) {
+  buckets[params.Bucket] = buckets[params.Bucket] || {};
+  const bucket = buckets[params.Bucket];
+  if (bucket[params.Key])
+  {
+    callback(null, JSON.parse(JSON.stringify(bucket[params.Key])));  
+  }
+  else
+  {
+    callback(new Error('not found'), null);
+  }
+  
+});
+
+AWS.mock('S3', 'upload', function (params, callback) {
+  buckets[params.Bucket] = buckets[params.Bucket] || {};
+  const bucket = buckets[params.Bucket];
+  bucket[params.Key] = {
+    LastModified: Date.now(),
+    Key: params.Key,
+    Body: JSON.parse(JSON.stringify(params.Body))
+  };
+  callback(null, 'done');
+});
+
+AWS.mock('S3', 'deleteObject', function (params, callback) {
+  buckets[params.Bucket] = buckets[params.Bucket] || {};
+  const bucket = buckets[params.Bucket];
+  delete bucket[params.Key];
+  callback(null, 'done');
+});
+
 const storageTester = require('./storageTester');
 const Storage = require('./S3Storage');
 
 storageTester(Storage, {
-  region: process.env.S3STORE_TEST_REGION,
-  connectionString: process.env.S3STORE_TEST_HOST
+  region: 'test',
+  connectionString: 'test'
 });
-}
